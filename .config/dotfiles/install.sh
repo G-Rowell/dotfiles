eb2304d8ed30a0d53fdae028b455212571dc0a72
Galen Rowell <growell3@gmail.com>
2022-02-19 23:36:39 +1100

    comment out troublesome neovim setup

--- .config/dotfiles/install.sh
+++ .config/dotfiles/install.sh
@@ -85,8 +85,8 @@ neovim_install() {
 
    # TODO: consider sub-modules for git?
    git clone https://github.com/G-Rowell/NvChad "$HOME_DIR/.config/nvim"
-   "$HOME_DIR/.local/bin/nvim.appimage" --headless -c 'autocmd User VimEnter quitall'
-   "$HOME_DIR/.local/bin/nvim.appimage" --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+   # "$HOME_DIR/.local/bin/nvim.appimage" --headless -c 'autocmd User VimEnter quitall'
+   # "$HOME_DIR/.local/bin/nvim.appimage" --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 }
 
